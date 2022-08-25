9cf1141e44a45caaeb67f665ad1b8c379397c82a
nabiladigo <nabiladigo91@gmail.com>
2022-08-25 17:35:52 -0400

    update

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from django.views import View 
 from django.views.generic.base import TemplateView
-# from .models import Country, City, Post, Comment, Profile
+# from .models import  Profile
+from .models import Country, City, Post, Comment, Profile
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse
@@ -20,55 +21,55 @@ class About(TemplateView):
     template_name = "about.html"
 
 
-class Post(TemplateView):
-    template_name = "post.html"
+# class Post(TemplateView):
+#     template_name = "post.html"
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        name = self.request.GET.get("name")
-        if name != None:
-            context["posts"] = Post.objects.filter(Country.objects.filter(user=self.request.user),name__icontains=name)
-            context["header"] = f"Searching for {name}"
-        else:
-            context["posts"] = Post.objects.filter(Country.objects.filter(user=self.request.user))
-            context["header"] = "The Perfect City to visit."
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         name = self.request.GET.get("name")
+#         if name != None:
+#             context["posts"] = Post.objects.filter(Country.objects.filter(user=self.request.user),name__icontains=name)
+#             context["header"] = f"Searching for {name}"
+#         else:
+#             context["posts"] = Post.objects.filter(Country.objects.filter(user=self.request.user))
+#             context["header"] = "The Perfect City to visit."
+#         return context
 
 
-class PostCreate(CreateView):
-    model = Post
-    fields = ['title', 'content','date']
-    template_name = "post_create.html"
+# class PostCreate(CreateView):
+#     model = Post
+#     fields = ['title', 'content','date']
+#     template_name = "post_create.html"
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(PostCreate, self).form_valid(form)
+#     def form_valid(self, form):
+#         form.instance.user = self.request.user
+#         return super(PostCreate, self).form_valid(form)
 
-    def get_success_url(self):
-        print(self.kwargs)
-        return reverse('post_detail', kwargs={'pk': self.object.pk})
+#     def get_success_url(self):
+#         print(self.kwargs)
+#         return reverse('post_detail', kwargs={'pk': self.object.pk})
 
-class PostDetail(DetailView):
-    model = Post
-    template_name = "post_detail.html"
+# class PostDetail(DetailView):
+#     model = Post
+#     template_name = "post_detail.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["posts"] = Post.objects.all()
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["posts"] = Post.objects.all()
+#         return context
 
-class PostUpdate(UpdateView):
-    model = Post
-    fields = ['title','content','date']
-    template_name = "post_update.html"
+# class PostUpdate(UpdateView):
+#     model = Post
+#     fields = ['title','content','date']
+#     template_name = "post_update.html"
 
-    def get_success_url(self):
-        return reverse('post_detail', kwargs={'pk': self.object.pk})
+#     def get_success_url(self):
+#         return reverse('post_detail', kwargs={'pk': self.object.pk})
 
-class PostDelete(DeleteView):
-    model = Post
-    template_name = "post_delete_confirmation.html"
-    success_url = "/posts/"
+# class PostDelete(DeleteView):
+#     model = Post
+#     template_name = "post_delete_confirmation.html"
+#     success_url = "/posts/"
 
 
 class Signup(View):
